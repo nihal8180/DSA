7a7c0b30dadcbb2c0687f0dc9b9f38c635e2257d
Nihal Singh <106116240+nihal8180@users.noreply.github.com>
2024-02-23 02:08:37 +0530

    Time: 141 ms (55.42%), Space: 72.5 MB (21.01%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,21 @@
+ class Solution 
+{
+public:
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        vector<int> judge(n+1);
+        
+        for(auto it:trust)
+        {
+            judge[it[0]]--; 
+            judge[it[1]]++; 
+        }
+        
+       
+        for(int i=1; i<=n; i++)
+            if(judge[i]==n-1) return i;
+        
+       
+        return -1;
+    }
+};
